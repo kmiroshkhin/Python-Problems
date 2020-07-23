d4eabd7bbfd63bfe32d1fc048a074ac9deb4622e
Kirill Miroshkhin <kmiroshkhin@gmail.com>
2020-07-23 16:01:44 -0700

    Add files via upload

---
+++ Medium_OOPCalculator.py
@@ -0,0 +1,18 @@
+'''
+Create methods for the Calculator class that can do the following:
+
+Add two numbers.
+Subtract two numbers.
+Multiply two numbers.
+Divide two numbers.
+'''
+
+class Calculator:
+    def add(a,b):
+        return a+b
+    def subtract(a,b):
+        return a-b
+    def multiply(a,b):
+        return a*b
+    def divide(a,b):
+        return a/b
