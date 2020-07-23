cead685a0bd2238c7eae87780f7e0944fc244523
Kirill Miroshkhin <kmiroshkhin@gmail.com>
2020-07-23 16:25:52 -0700

    Add files via upload

--- Medium_OOPCalculator.py
+++ Medium_OOPCalculator.py
@@ -8,11 +8,14 @@ Divide two numbers.
 '''
 
 class Calculator:
-    def add(a,b):
+
+    def add(self,a,b):
         return a+b
-    def subtract(a,b):
+    def subtract(self,a,b):
         return a-b
-    def multiply(a,b):
+    def multiply(self,a,b):
         return a*b
-    def divide(a,b):
+    def divide(self,a,b):
         return a/b
+
+
