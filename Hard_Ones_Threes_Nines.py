8996edf39b2506992305396292ba890ee6695c77
Kirill Miroshkhin <kmiroshkhin@gmail.com>
2020-08-02 13:54:11 -0700

    Add files via upload

---
+++ Hard_Ones_Threes_Nines.py
@@ -0,0 +1,25 @@
+"""
+Ones, Threes and Nines (Version #2)
+Given an integer between 0 and 26, make a variable (self.answer). That variable would be assigned to a string in this
+format:
+
+"nines:your answer, threes:your answer, ones:your answer"
+You need to find out how many ones, threes, and nines it would at least take for the number of each to add up to the
+given integer when multiplied by one, three or nine (depends).
+
+Examples
+ones_threes_nines(10) ➞ "nines:1, threes:0, ones:1"
+
+ones_threes_nines(15) ➞ "nines:1, threes:2, ones:0"
+
+ones_threes_nines(22) ➞ "nines:2, threes:1, ones:1"
+"""
+
+
+class ones_threes_ninestwo:
+    def __init__(self, num):
+        self.nines = num // 9
+        self.threes = (num - self.nines * 9) // 3
+        self.ones = (num - self.nines * 9 - self.threes * 3)
+        self.answer = 'nines:' + str(self.nines) + ', threes:' + str(self.threes) + ', ones:' + str(self.ones)
+
